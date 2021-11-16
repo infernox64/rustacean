fc586fa0d438b883b69071b3eb0269bfc1e2ebb5
Infernox64 <infernox64@gmail.com>
2021-11-16 17:23:35 -0500

    suck a thousand dicks && die;

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use windows::{
+    runtime::*, Data::Xml::Dom::*, Win32::Foundation::*, Win32::System::Threading::*,
+    Win32::UI::WindowsAndMessaging::*,
+    Data::Json::*,
+    Win32::Storage::FileSystem::*,
+    Win32::System::WinRT::Shell::*,
+    Win32::System::IO::*
+};
+
+
+fn main() {
+    println!("Hello, world!");
+}
