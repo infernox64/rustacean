f79ebfb23beeb26f9d1a57e9ede2462130804da0
Infernox64 <infernox64@gmail.com>
2021-11-17 12:51:26 -0500

    sniff my ass

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,44 @@
-use windows::{
-    runtime::*, Data::Xml::Dom::*, Win32::Foundation::*, Win32::System::Threading::*,
-    Win32::UI::WindowsAndMessaging::*,
-    Data::Json::*,
-    Win32::Storage::FileSystem::*,
-    Win32::System::WinRT::Shell::*,
-    Win32::System::IO::*
-};
+#![allow(incomplete_features)]
+#![feature(unsized_locals, unsized_fn_params)]
 
+use std::env;
+use commander;
+use std::thread;
+use std::time::Duration;
+use win32_notification::NotificationBuilder;
+struct notif_config {
+    title: &str
+    text: &str
+}
 
+fn notify(config: notif_config) {
+    let notification = NotificationBuilder::new()
+    .title_text(config.title)
+    .info_text(config.text)
+    .build()
+    .expect("Could not create notification");
+    notification.show().expect("Failed to show notification");
+    thread::sleep(Duration::from_secs(5));
+    notification
+        .delete()
+        .expect("Failed to delete notification");
+}
+impl notif_config {
+    fn new(&[String]) -> notif_config {
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
+        let tit = args[1].clone();
+        let txt = args[2].clone();
+        notif_config {&tit, &txt}
+    }
+    
+}
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+cfg: notif_config = parse_args(&args);
+    //    let tit: &str = &args[1];
+//    let txt: &str = &args[2];
+    
+    notify(&cfg);
 }
